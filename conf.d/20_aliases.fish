f58083e95ac353181e242d4f81cbfbbdf9742545
hansel <58466533+JHXs@users.noreply.github.com>
2026-01-17 21:43:53 +0800

    添加rm确认别名

--- conf.d/20_aliases.fish
+++ conf.d/20_aliases.fish
@@ -1,5 +1,7 @@
 # 用于设置命令别名。
 
+alias rm='rm -i'
+
 # clash tun 开关
 alias tun="curl http://127.0.0.1:8090/configs -X PATCH -H 'Authorization: Bearer ikunji' -d '{\"tun\": {\"enable\": true}}'"
 alias tunno="curl http://127.0.0.1:8090/configs -X PATCH -H 'Authorization: Bearer ikunji' -d '{\"tun\": {\"enable\": false}}'"
